0c4ef150c673bfa4016542371c9342d17565327e
TenSkyline <rayvaldokncc@gmail.com>
2024-03-22 16:33:34 +0700

    add navigation icon to AppBar

--- app/src/main/java/com/tenskyline/mywishlistapp/HomeView.kt
+++ app/src/main/java/com/tenskyline/mywishlistapp/HomeView.kt
@@ -1,17 +1,22 @@
 package com.tenskyline.mywishlistapp
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
     Scaffold(
         topBar = {
-            AppBarView(title = "WishList")
+            AppBarView(title = "WishList", {
+                Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            })
         }
     ) {
         LazyColumn(
