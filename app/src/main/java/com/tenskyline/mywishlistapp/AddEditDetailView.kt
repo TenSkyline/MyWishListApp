1a98bb89f9a80ff0ddfed5d9c943a56adc5d98ea
TenSkyline <rayvaldokncc@gmail.com>
2024-03-26 15:26:32 +0700

    edit AddEditDetailView.kt design own OutlinedTextField

--- app/src/main/java/com/tenskyline/mywishlistapp/AddEditDetailView.kt
+++ app/src/main/java/com/tenskyline/mywishlistapp/AddEditDetailView.kt
@@ -3,14 +3,25 @@ package com.tenskyline.mywishlistapp
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -46,3 +57,34 @@ fun AddEditDetailView(
     }
 }
 
+
+@Composable
+fun WishTextField(
+    label: String,
+    value: String,
+    onValueChanged: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChanged,
+        label = { Text(text = label, color = Color.Black) },
+        modifier = Modifier.fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedTextColor = colorResource(id = R.color.black),
+            unfocusedTextColor = colorResource(id = R.color.black),
+            focusedBorderColor = colorResource(id = R.color.black),
+            unfocusedBorderColor = colorResource(id = R.color.black),
+            focusedLabelColor = colorResource(id = R.color.black),
+            unfocusedLabelColor = colorResource(id = R.color.black),
+            cursorColor = colorResource(id = R.color.black)
+
+        )
+    )
+}
+
+@Preview
+@Composable
+fun WishTestFieldPrev(){
+    WishTextField(label = "text", value = "abc", onValueChanged = {})
+}
